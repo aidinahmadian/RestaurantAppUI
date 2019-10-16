4d377e6e1d14cf5297271579531d7878314caf91
Aidin Ahmadian <ahmadian_aidin@yahoo.com>
2019-10-16 19:39:54 +0330

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by aidin ahmadian on 10/16/19.
+//  Copyright © 2019 aidin ahmadian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
